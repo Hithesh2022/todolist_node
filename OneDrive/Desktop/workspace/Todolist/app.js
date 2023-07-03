87a232b5b2b043f3c2b5b9c24c6ac5cd852e4601
Hithesh2022 <Hith68616@gmail.com>
2023-07-03 21:41:51 +0530

    completed todolist

--- OneDrive/Desktop/workspace/Todolist/app.js
+++ OneDrive/Desktop/workspace/Todolist/app.js
@@ -1,31 +1,27 @@
-const express =require("express");
-const bodyparser=require("body-parser");
+const express=require("express");
+const body=require("body-parser");
 const app=express();
+let items=["buy food","cook","Eat food"];
 app.set('view engine',"ejs");
+app.use(express.static('public'));
+app.use(body.urlencoded({extended:true}));
 app.get("/",function(req,res){
 let today=new Date();
-let currentDay=today.getDay();
-let day="";
-switch(currentDay)
-{
-case 1: day="monday";
-        break;
-case 2: day="tuesday";
-        break;
-case 3 :day="wednesday";
-        break;
-case 4 :day="thursday";
-        break;
-case 5: day="friday";
-        break;
-case 6 :day="Saturday";
-        break;
-case 7:day="Sunday";
-break;
-default:console.log("Invalid Day");
+let optiions={
+        weekday:"long",
+        day:"numeric",
+        month:"long"
+};
+let day=today.toLocaleDateString("en-US",optiions);
 
-}
-res.render("list",{kindofday:day});
+
+res.render("list",{kindofday:day,newlistitems:items});
+})
+app.post("/",function(req,res){
+let item=req.body.newitem;
+res.redirect("/");
+items.push(item);
+
 })
 app.listen(3000,function(){
     console.log("server is running");
